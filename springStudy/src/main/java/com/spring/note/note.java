19433a5e244d543f4cd01305223909e0dca8c55c
hjh <rkgh17@gmail.com>
2023-05-26 17:10:33 +0900

    merge

---
+++ springStudy/src/main/java/com/spring/note/note.java
@@ -0,0 +1,22 @@
+package com.spring.note;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class note {
+	
+	public static void main(String[] args) {
+		
+		Date now = new Date();
+		String strFormatter = new SimpleDateFormat("yyyy-MM-dd").format(new Date(System.currentTimeMillis()));
+		
+//		System.out.println(now);
+//		System.out.println(System.currentTimeMillis());
+//		System.out.println(new Date(System.currentTimeMillis()));
+//		System.out.println(strFormatter);
+		
+		
+		
+	}
+	
+}
